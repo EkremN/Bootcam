fd71ca63aa8523380ed3d53a947b8de6ad26dc2c
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-07-27 10:36:16 +0100

    Age in days solution

---
+++ week_1/day_2/challenges/03_ages_in_days/age_in_days.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# This "require" line loads the contents of the 'date' file from the standard
+# Ruby library, giving you access to the Date class.
+require 'date'
+
+def age_in_days(day, month, year)
+  # TODO: return the age expressed in days
+  # Given the day, month, and year of birth
+  today = Date.today
+  user_input = Date.new(year, month, day)
+  (today - user_input).to_i
+end
