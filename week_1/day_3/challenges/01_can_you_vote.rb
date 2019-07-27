c01b8b66e78f8fc4ef461f8b54654ec315c60a6e
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-07-27 19:28:39 +0100

    ca you vote challenge

---
+++ week_1/day_3/challenges/01_can_you_vote.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+def allowed_to_vote?(age)
+  # TODO: return a boolean stating whether `age` is old enough to vote
+  # NOTE: Use an if/else statment
+  age >= 18
+end
