6edfb9082fa5eb7fc8fabb7d588a0366ac0692e6
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-07-30 17:00:44 +0100

    map

---
+++ week_1/day_4/lesson/map.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+musicians = [
+  'David Gilmour',
+  'Roger Waters',
+  'Richard Wright',
+  'Nick Mason'
+]
+
+first_name = musicians.map do |musician|
+  musician.split.first
+end
+
+last_name = musicians.map do |musician|
+  musician.split.last
+end
+
+firs_name_surname = musicians.map do |musician|
+  first_letter = musician.split.first[0]
+  last_letter = musician.split.last[0]
+  first_letter + last_letter
+end
+
+p firs_name_surname[0]
+p firs_name_surname[1]
+
+p first_name
+p last_name
