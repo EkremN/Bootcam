353147e8514db405faf21236b723cd888a8e51b7
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-03 21:54:26 +0100

    Live code encrypt

--- week_1/day_4/live_code/encrypt.rb
+++ week_1/day_4/live_code/encrypt.rb
@@ -1,27 +1,30 @@
 # frozen_string_literal: true
 
 def encrypt(sentence)
-  alphabet = ("A".."Z").to_a
-  shift_alphabet = ("X".."Z").to_a + ("A".."W").to_a
+  alphabet = ('A'..'Z').to_a
   sentence.chars.map do |char|
-    # "#{index}. #{char}"
-    if char == " "
+    # '#{index}. #{char}'
+    if char == ' '
       char
     else
-      char = alphabet[alphabet.index(char) - 3]
+      # *******************************************
+      p char = alphabet[alphabet.index(char) - 3] # I dont get it
+      # *******************************************
       char
     end
   end.join
 end
 
-puts encrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG") == "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
+puts encrypt('EKREM')
 
 describe "#encrypt" do
 
-  it "return QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD an empty string when passed THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG" do
+    it "return QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD an empty string when passed THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG" do
 
     actual = encrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG")
     expected = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
 
     expect(actual).to eq(expected)
   end
+end
+x
