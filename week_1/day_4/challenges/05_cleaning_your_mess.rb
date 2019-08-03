a20446334b69cd25e7f58b7234b28dd524e3ef51
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-03 13:02:34 +0100

    Cleaning your mess

---
+++ week_1/day_4/challenges/05_cleaning_your_mess.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+def hop_hop_hop(number_of_exercises)
+  counter = []
+  (1..number_of_exercises).each do |_numb|
+    counter << 'hop!'
+    puts counter.join(' ') + ' One more time...'
+  end
+end
+
+hop_hop_hop(6)
